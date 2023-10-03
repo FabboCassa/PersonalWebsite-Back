d7908f39a11e074c25477a3788e60951962959aa
Fabio <casarini.fabio@gmail.com>
2023-10-03 17:47:28 +0200

    Created app website and added to settings.py

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+
+from website.models import Project
+
 
 # Create your views here.
+class ProjectsListView (generic.ListView):
+    model = Project
